b2de85f7532bf5e1c53443e8fe4b20b1cbc90c35
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-10 16:11:38 -0300

    Logica base del registro implementado

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  nombre: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      console.log("Las contraseñas no coinciden.");
+      return;
+    }
+    console.log("Nombre: ", this.nombre);
+    console.log("Email: ", this.email);
+    console.log("Contraseña: ", this.password);
+  }
 }
