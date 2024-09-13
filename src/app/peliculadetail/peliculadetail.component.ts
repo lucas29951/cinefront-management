8bf119c800a13876406eb784a8166cb27ec9cdad
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-13 20:55:57 -0300

    Componente agregado para los detalles de peliculas

---
+++ src/app/peliculadetail/peliculadetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peliculadetail',
+  templateUrl: './peliculadetail.component.html',
+  styleUrls: ['./peliculadetail.component.css']
+})
+export class PeliculadetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
