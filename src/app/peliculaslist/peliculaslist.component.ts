96163c28978855a69fa2880754a2916df6253617
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-13 20:33:06 -0300

    Componente agregado para la vista de las peliculas

---
+++ src/app/peliculaslist/peliculaslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peliculaslist',
+  templateUrl: './peliculaslist.component.html',
+  styleUrls: ['./peliculaslist.component.css']
+})
+export class PeliculaslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
