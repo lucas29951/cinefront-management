d7eb8e782dc20ba160a1cfd43591e9a791eb7737
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-13 20:50:16 -0300

    Logica implementada para el componente

--- src/app/peliculaslist/peliculaslist.component.ts
+++ src/app/peliculaslist/peliculaslist.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peliculaslist.component.css']
 })
 export class PeliculaslistComponent implements OnInit {
+  peliculas = [
+    {
+      id: 1,
+      titulo: 'Película 1',
+      imagen: 'https://via.placeholder.com/400x300',
+      genero: 'Acción',
+      duracion: 120,
+      descripcion: 'Una película llena de acción y aventura.',
+      fechaEstreno: '2024-09-15'
+    },
+    {
+      id: 2,
+      titulo: 'Otra Película',
+      imagen: 'https://via.placeholder.com/400x300',
+      genero: 'Comedia',
+      duracion: 90,
+      descripcion: 'Una comedia que te hará reír a carcajadas.',
+      fechaEstreno: '2024-10-01'
+    },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  verDetalles(id: number) {
+    console.log('Ver detalles de pelicula con ID: ', id);
+  }
+
+  comprarEntradas(id: number) {
+    console.log('Comprar entradas para la pelicula con ID: ', id);
+    
+  }
 }
