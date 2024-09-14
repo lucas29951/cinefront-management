4e4bcc062a9b03a5515e6f93f237c5b7de086c9e
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-14 18:36:02 -0300

    Implementada la logica base de la vista

--- src/app/funcioneslist/funcioneslist.component.ts
+++ src/app/funcioneslist/funcioneslist.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcioneslist.component.css']
 })
 export class FuncioneslistComponent implements OnInit {
+  funciones = [
+    {
+      id: 1,
+      peliculaTitulo: 'Película Ejemplo 1',
+      fecha: '2024-09-15',
+      hora: '18:00',
+      sala: 'Sala 1'
+    },
+    {
+      id: 2,
+      peliculaTitulo: 'Película Ejemplo 2',
+      fecha: '2024-09-16',
+      hora: '20:00',
+      sala: 'Sala 2'
+    },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  volverAInicio() {
+    console.log('Volver a la página principal');
+  }
+
+  comprarEntradas(funcionId: number) {
+    console.log('Comprar entradas para la función con ID:', funcionId);
+  }
 }
