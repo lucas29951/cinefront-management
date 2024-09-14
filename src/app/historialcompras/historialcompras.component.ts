5abe2b43098397509d1bacae00be50aa8550f83a
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-14 19:00:08 -0300

    Logica de la vista implementada

--- src/app/historialcompras/historialcompras.component.ts
+++ src/app/historialcompras/historialcompras.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Compra {
+  peliculaTitulo: string;
+  fechaFuncion: string;
+  horaFuncion: string;
+  cantidadEntradas: number;
+  fechaCompra: string;
+  total: number;
+}
+
 @Component({
   selector: 'app-historialcompras',
   templateUrl: './historialcompras.component.html',
   styleUrls: ['./historialcompras.component.css']
 })
 export class HistorialcomprasComponent implements OnInit {
+  historialCompras: Compra[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.historialCompras = this.obtenerHistorialCompras();
   }
 
+  obtenerHistorialCompras(): Compra[] {
+    return [
+      {
+        peliculaTitulo: 'Película Ejemplo 1',
+        fechaFuncion: '2024-09-20',
+        horaFuncion: '18:00',
+        cantidadEntradas: 2,
+        fechaCompra: '2024-09-15',
+        total: 20.00
+      },
+      {
+        peliculaTitulo: 'Película Ejemplo 2',
+        fechaFuncion: '2024-09-21',
+        horaFuncion: '20:00',
+        cantidadEntradas: 3,
+        fechaCompra: '2024-09-16',
+        total: 30.00
+      }
+    ];
+  }
 }
