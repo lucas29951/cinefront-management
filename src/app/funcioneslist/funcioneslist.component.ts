604e2ef0795486ebc3ebb8aac6a41e35517c6715
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-14 18:25:58 -0300

    Componente de las funciones incorporada

---
+++ src/app/funcioneslist/funcioneslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcioneslist',
+  templateUrl: './funcioneslist.component.html',
+  styleUrls: ['./funcioneslist.component.css']
+})
+export class FuncioneslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
