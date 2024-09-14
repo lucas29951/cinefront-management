4389f74bb99e896a96882a0a0e433236154827e8
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-14 18:48:47 -0300

    Componente agregado para la vista de Historial

---
+++ src/app/historialcompras/historialcompras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historialcompras',
+  templateUrl: './historialcompras.component.html',
+  styleUrls: ['./historialcompras.component.css']
+})
+export class HistorialcomprasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
