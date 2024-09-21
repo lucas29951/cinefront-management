fadd521ebcaf25d8d356241c1780629b7d732a92
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-21 17:30:43 -0300

    HttpClientModule agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { HistorialcomprasComponent } from './historialcompras/historialcompras.c
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
