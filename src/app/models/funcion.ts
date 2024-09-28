731963db07b30935d37f732b9290f7108e35cd65
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-28 19:53:24 -0300

    Modelo para las Funciones agregado

---
+++ src/app/models/funcion.ts
@@ -0,0 +1,11 @@
+
+export interface Funcion {
+    id: number;
+    id_pelicula: number;
+    fecha: string;
+    hora: string;
+    sala: string;
+    precio: number;
+    asientos_disponibles: number;
+  }
+  
