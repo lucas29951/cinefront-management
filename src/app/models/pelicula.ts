87bd68f43d7cdd1a5f036b733a41c6446ac57283
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-28 19:50:30 -0300

    Se agrego la carpeta de modelos - Pelicula

---
+++ src/app/models/pelicula.ts
@@ -0,0 +1,12 @@
+
+export interface Pelicula {
+    id: number;
+    titulo: string;
+    descripcion: string;
+    director: string;
+    duracion: number;
+    genero: string;
+    clasificacion: number;
+    poster_url: string;
+  }
+  
