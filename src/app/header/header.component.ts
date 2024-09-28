991e27c88f55754e423457d029f08d0b28b97f96
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-27 21:25:32 -0300

    Se agrego la funcionalidad para detectar los cambios en las variables de usuario

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -12,12 +13,14 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
     this.initializeNavbarBurger();
     this.nombreUsuario = localStorage.getItem('nombreUsuario');
+    this.cdr.detectChanges();
   }
 
   initializeNavbarBurger() {
@@ -41,7 +44,9 @@ export class HeaderComponent implements OnInit {
   logout() {
     this.authService.logout();
     localStorage.removeItem('nombreUsuario');
+    localStorage.removeItem('correoUsuario');
     this.nombreUsuario = null;
+    this.cdr.detectChanges();
     this.router.navigate(['/login']);
   }
 }
