efe83320bbd561a062a33c865475f43944f257c6
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-09-29 19:51:27 -0300

    Actualizacion del service de Peliculas

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Pelicula } from '../models/pelicula';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +15,11 @@ export class PeliculasService {
     return this.http.get(`${this.apiUrl}`);
   }
 
-  addPelicula(pelicula: any): Observable<any> {
+  addPelicula(pelicula: Pelicula): Observable<any> {
     return this.http.post(this.apiUrl, pelicula);
   }
 
-  updatePelicula(id: string, pelicula: any): Observable<any> {
+  updatePelicula(id: string, pelicula: Pelicula): Observable<any> {
     return this.http.put(`${this.apiUrl}/${id}`, pelicula);
   }
 
