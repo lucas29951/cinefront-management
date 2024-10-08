546170f67f41d2a72ed433ff7034c6d20fce7413
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-10-08 17:32:37 -0300

    Modelo de Compra agregado

---
+++ src/app/models/compra.ts
@@ -0,0 +1,9 @@
+
+export interface Compra {
+    peliculaTitulo: string;
+    fechaFuncion: string;
+    horaFuncion: string;
+    cantidadEntradas: number;
+    fechaCompra: string;
+    total: number;
+  }
